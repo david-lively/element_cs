d991cbb5652dda8e6c41034cbff2049ede389952
David Lively <davidlively@gmail.com>
2025-03-23 16:33:41 -0500

    Added some tests.

--- main.cpp
+++ main.cpp
@@ -9,17 +9,51 @@ bool inRange(const Vec2& vec, float mn, float mx) {
 }
 
 void runTests(const Vec2& mapDims, const vector<unsigned char>& beforeData, const vector<unsigned char>& afterData) {
-    Vec2 p0(0,0);
-    Vec2 p1(255,255);
-    Vec2 p2(512,512);
 
-    float d01 = Analyzer::CalculatePathLength(beforeData, mapDims, p0,p1);
-    float d12 = Analyzer::CalculatePathLength(beforeData, mapDims, p1,p2);
-    float d02 = Analyzer::CalculatePathLength(beforeData,mapDims,p2,p0);
+    {
+        cout << "Split diagonal test" << endl;
+        Vec2 p0(0,0);
+        Vec2 p1(255,255);
+        Vec2 p2(511,511);
+        cout << "P0 " << p0.x << "," << p0.y << endl;
+        cout << "P1 " << p1.x << "," << p1.y << endl;
+        cout << "P2 " << p2.x << "," << p2.y << endl;
 
-    cout << "d01 = " << d01 << " d12 = " << d12 << " d02 = " << d02 << endl;
-    float delta = (d02 - (d01 + d12));
-    cout << "delta = " << delta << endl;
+        float d01 = Analyzer::CalculatePathLength(beforeData, mapDims, p0,p1);
+        float d12 = Analyzer::CalculatePathLength(beforeData, mapDims, p1,p2);
+        float d02 = Analyzer::CalculatePathLength(beforeData,mapDims,p2,p0);
+
+        cout << "d01 = " << d01 << " d12 = " << d12 << " d02 = " << d02 << endl;
+        float error = d02 - d01 - d12;
+        cout << "error = " << error << endl;
+    }
+
+    cout << "\n\n";
+
+    {
+        Vec2 start(0,0);
+        Vec2 end(511,0);
+        float cp0 = 0;
+        float cp1 = 0;
+        for (int i=start.x + 1; i <= end.x; ++i) {
+            float d0 = 11.0f * abs(beforeData[i] - beforeData[i-1]);
+            cp0 += sqrt(30*30+d0*d0);
+            float d1 = 11.0f * abs(afterData[i] - afterData[i-1]);
+            cp1 += sqrt(30*30 + d1*d1);
+        }
+
+        cout << " Horizontal test\n: Before: " << cp0 << " after " << cp1 << " delta " << (cp1 - cp0) << endl;
+        float ap0 = Analyzer::CalculatePathLength(beforeData,mapDims,start, end);
+        float ap1 = Analyzer::CalculatePathLength(afterData,mapDims,start,end);
+        cout << "Calculated:\n  Before: " << ap0 << " after " << ap1 << " delta " << ap1 - ap0 << endl;
+
+        cout << "Compare Horizontal test from " << start.x << "," << start.y << " to " << end.x << "," << end.y << endl;
+        cout << "Before\tAfter\tDelta\n";
+        cout << cp0 << "\t" << cp1 << "\t" << (cp0 - cp1) << endl;
+        cout << ap0 << "\t" << ap1 << "\t" << (ap0 - ap1) << endl;
+        cout << "-----------------------------------------------------" << endl;
+        cout << (cp0 - ap0) << "\t" << (cp1 - ap1) << "\t" << (cp1 - cp0) - (ap1 - ap0) << endl;
+    }
 }
 
 int main(int argc, char** argv)
